c42edf014144c6414e397574249621a25308f13d
Christoph Zuleger <christoph.zuleger@gmail.com>
2023-04-12 17:12:49 +0200

    💄 Loading logic improvements.

--- frontend/projects/form/src/app/pages/start/start.component.ts
+++ frontend/projects/form/src/app/pages/start/start.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {environment} from "../../../environments/environment";
-import {Subject, takeUntil} from "rxjs";
+import {BehaviorSubject, Subject, takeUntil} from "rxjs";
 import {Project, ProjectService} from "../../../../../shared/src/lib/projects/project.service";
 import {ProjectInfoComponent} from "../../../../../shared/src/lib/projects/project-info.component";
 import {ProjectListComponent} from "../../../../../shared/src/lib/projects/project-list.component";
@@ -15,20 +15,30 @@ import {Router, RouterModule} from "@angular/router";
   styles: []
 })
 export class StartComponent implements OnInit, OnDestroy {
-  protected _unsubscribe$ = new Subject<void>();
-  protected apiUrl = environment.apiUrl;
-  protected projects: ReadonlyArray<Project.Info> = [];
+  protected readonly _unsubscribe$ = new Subject<void>();
+
+  protected readonly _projectListCtrl$ = new BehaviorSubject<Project.Info[]>([]);
+  protected readonly projectList = this._projectListCtrl$.asObservable();
+  protected readonly _projectListLoadedCtrl$ = new BehaviorSubject<boolean>(false);
+  protected readonly projectListLoaded = this._projectListLoadedCtrl$.asObservable();
 
   constructor(protected router: Router, protected projectService: ProjectService) {
   }
 
   ngOnInit(): void {
     // load known projects
+    this._projectListLoadedCtrl$.next(false);
     this.projectService.loadKnown(environment.apiUrl)
       .pipe(takeUntil(this._unsubscribe$))
       .subscribe({
-        next: projects => this.projects = projects,
-        error: err => console.warn(`[StartComponent] Failed to load known projects.`, err),
+        next: projects => {
+          this._projectListCtrl$.next(projects)
+          this._projectListLoadedCtrl$.next(true);
+        },
+        error: err => {
+          console.warn(`[StartComponent] Failed to load known projects.`, err)
+          this._projectListLoadedCtrl$.next(true);
+        },
       });
   }
 
